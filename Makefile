5751e77f140427bf13d9e37671df04a6c203398c
rdcm <rdcm@protonmail.ch>
2025-04-06 00:26:01 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+format:
+	cargo fmt
+
+up:
+	drivers/chromedriver-mac-x64/chromedriver --port=9515
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use anyhow::Result;
+use scraper::{Html, Selector};
+use std::time::Duration;
+use tokio::time::sleep;
+use urlencoding::encode;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let timeout_sec = 10;
+    let rust_jobs_url = "https://rustjobs.dev";
+    let raw_html = open_page(rust_jobs_url, "http://localhost:9515", timeout_sec).await?;
+    let job_links = get_links(&rust_jobs_url, &raw_html)?;
+    let inserted_links = persist_links(&job_links)?;
+
+    for inserted_link in inserted_links {
+        send_message(
+            &std::env::var("BOT_TOKEN")?,
+            &std::env::var("CHAT_ID")?,
+            &inserted_link,
+        )
+        .await?;
+
+        sleep(Duration::from_secs(1)).await;
+    }
+
+    Ok(())
+}
+
+async fn send_message(token: &str, chat_id: &str, text: &str) -> Result<()> {
+    let message = format!("{text} \n\n#rust_jobs");
+    let encoded_message = encode(&message);
+    let url = format!(
+        "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}",
+        token, chat_id, encoded_message
+    );
+
+    reqwest::Client::new().post(&url).send().await?;
+
+    Ok(())
+}
+
+async fn open_page(url: &str, browser_url: &str, timeout: u64) -> Result<String> {
+    let c = fantoccini::ClientBuilder::native()
+        .connect(browser_url)
+        .await?;
+    c.goto(url).await?;
+    sleep(Duration::from_secs(timeout)).await;
+    let page_html = c.source().await?;
+
+    c.close().await?;
+
+    Ok(page_html)
+}
+
+fn get_links(base_url: &str, page_html: &str) -> Result<Vec<String>> {
+    let mut links = Vec::new();
+    let document = Html::parse_document(&page_html);
+    let selector = Selector::parse(r#"a[eventcategory="Featured Job Title"]"#)
+        .expect("failed to create selector");
+    for a in document.select(&selector) {
+        let href = a.value().attr("href").unwrap_or("#");
+        links.push(format!("{base_url}{href}"));
+    }
+
+    Ok(links)
+}
+
+fn persist_links(links: &Vec<String>) -> Result<Vec<String>> {
+    let mut inserted_links = Vec::new();
+    let db = sled::open("rust_jobs")?;
+    for link in links {
+        match db.get(link) {
+            Ok(vec_opt) => match vec_opt {
+                Some(_) => {
+                    println!("Already exists: {:?}", link);
+                }
+                None => {
+                    _ = db.insert(link, link.as_bytes())?;
+                    inserted_links.push(link.to_string());
+                    println!("Inserted: {:?}", link);
+                }
+            },
+            Err(err) => {
+                println!("Reading database error: {:?}", err);
+            }
+        }
+    }
+
+    Ok(inserted_links)
+}
